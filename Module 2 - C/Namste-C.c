25c2ca20a2df50974089ab1291ff354debddb082
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-07-18 18:36:08 +0530

    Module 2 Lecture 1 18-7

---
+++ Module 2 - C/Namste-C.c
@@ -0,0 +1,11 @@
+
+// Pre-processor, it wil be loaded before your program executes
+#include <stdio.h>
+
+
+// void main function, which marks start of current program
+void main(){
+    printf("Namste C Language");
+    printf("\nHello World");
+    // This line will be ignored
+}
---
+++ Module 2 - C/variable-18-7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main(){
+
+    // syntax
+
+    // Variable declaration
+
+    // return_type variable_name;
+    int num;
+
+    // variable initialization
+    num=25;
+
+
+    // Variable declaration + initialization
+
+    // return_type variable_name = variable_value;
+    int some=32;
+
+    // Variable value update
+    some=45;
+
+
+    int number = 9432;
+    printf("Number is : %d",number);
+    number=531;
+    printf("\nNumber is : %d",number);
+}
